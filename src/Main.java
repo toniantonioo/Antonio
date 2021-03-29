322c24fd120942c2c681a482546fc3357785000c
“chiciudean <toni.antonioo44@gmail.com>
2021-03-29 21:03:28 +0300

    tema

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello" );
+        System.out.println(("1.")+"hello" );
         System.out.println("Antonio" );
         System.out.println( ("2.")+2+5 );
         System.out.println( ("3.")+10/5 );
-        System.out.println( ("a.")+-5+8*6 );
-        System.out.println( ("b.")+(55+9) % 9 );
-        System.out.println( ("c.")+20 + -3*5/8);
-        System.out.println( ("d.")+(5+15/3*2-8%3 ));
+        System.out.println( ("4a.")+-5+8*6 );
+        System.out.println( ("4b.")+(55+9) % 9 );
+        System.out.println( ("4c.")+20 + -3*5/8);
+        System.out.println( ("4d.")+(5+15/3*2-8%3 ));
 
     }
 }
